0c02be53726869424fe270704920fa7bd2554962
Juan-seb <angulocita@gmail.com>
2022-10-08 12:31:14 -0500

    Login on the component InputNames ready

--- ejercicio_dos/src/App.jsx
+++ ejercicio_dos/src/App.jsx
@@ -1,9 +1,9 @@
-import {useState} from 'react'
+import { useState } from 'react'
 import InputNames from './components/InputNames'
 import './App.css'
 
 function App() {
-  
+
   const [names, setNames] = useState([])
 
   const handleSubmit = (e) => {
@@ -13,18 +13,22 @@ function App() {
   }
 
   return (
-    <div className="app">
-      <h2>Predictor de edad</h2>
-      Instrucciones:
-      <ul>
-        <li className='app_li'>Puedes ingresar cuantos nombres desees separados por comas (',') o un unico nombre</li>
-        <li className='app_li'>El campo localización es opcional por lo tanto no es necesario que marques el país.</li>
-        <li className='app_li'>Los nombres deben ser unicos, es decir no puedes ingresar: Juan Sebastian</li>
-      </ul>
-      <form className='app_form' onSubmit={handleSubmit}>
-        <InputNames/>
-      </form>
-    </div>
+    <main className="app">
+      <section className='app_container'>
+        <h2 className='app_title'>Predictor de edad</h2>
+        Instrucciones:
+        <ul>
+          <li className='app_li'>Puedes ingresar cuantos nombres desees separados por comas (',') o un unico nombre</li>
+          <li className='app_li'>El campo localización es opcional por lo tanto no es necesario que marques el país.</li>
+          <li className='app_li'>Los nombres deben ser unicos, es decir no puedes ingresar: Juan Sebastian</li>
+        </ul>
+        <form className='app_form' onSubmit={handleSubmit}>
+          <main>
+            <InputNames names={names} setNames={setNames} />
+          </main>
+        </form>
+      </section>
+    </main>
   )
 }
 
--- ejercicio_dos/src/components/InputNames/index.jsx
+++ ejercicio_dos/src/components/InputNames/index.jsx
@@ -1,7 +1,84 @@
-const InputNames = () => {
+import { useState } from 'react'
+import './style.css'
 
-  return(
-    <input type="text" className="input_name"/>
+const InputNames = ({ names, setNames }) => {
+
+  const [inputName, setInputName] = useState({
+    value: ''
+  })
+
+  const [errorName, setErrorName] = useState(false)
+
+  const handleChange = (e) => {
+
+    const value = e.target.value
+    const lastCharacter = value.charAt(value.length - 1)
+    const name = value.substring(0, value.length - 1)
+    const regexName = /^[a-zA-Z]+$/
+
+    if (!(lastCharacter === ',')) {
+      setInputName({ value })
+      return
+    }
+
+    console.log(regexName.test(name))
+    if (!(regexName.test(name))) {
+
+      setErrorName(true)
+
+      setInterval(() => {
+        setErrorName(false)
+      }, 1000);
+
+      return
+    }
+
+    setNames([...names, name])
+    setInputName({
+      value: ''
+    })
+
+
+  }
+
+  const handleBlur = (e) => {
+
+    const value = e.target.value
+    const regexName = /^[a-zA-Z]+$/
+
+    if (!(regexName.test(value))) {
+
+      setErrorName(true)
+
+      setInterval(() => {
+        setErrorName(false)
+      }, 1000);
+
+      return
+    }
+
+    setNames([...names, value])
+    setInputName({
+      value: ''
+    })
+
+  }
+
+  return (
+    <section>
+      <article className='app_form_article'>
+        <label htmlFor="input_names">Ingresa aqui los nombres:</label>
+        <input
+          type="text"
+          className="app_form_input"
+          name="input_names"
+          value={inputName.value}
+          onChange={handleChange}
+          onBlur={handleBlur}
+        />
+      </article>
+      {errorName && <p>No se admiten campos vacios, ni espacios, ni caracteres especiales</p>}
+    </section>
   )
 
 }
