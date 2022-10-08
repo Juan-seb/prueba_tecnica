814c2f30c14eaf31737b276d049b0ac7762afb0e
Juan-seb <angulocita@gmail.com>
2022-10-08 11:27:16 -0500

    Working on the logic of the age predictor

--- ejercicio_dos/src/App.jsx
+++ ejercicio_dos/src/App.jsx
@@ -1,32 +1,29 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
+import {useState} from 'react'
+import InputNames from './components/InputNames'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  
+  const [names, setNames] = useState([])
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+
+
+  }
 
   return (
-    <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+    <div className="app">
+      <h2>Predictor de edad</h2>
+      Instrucciones:
+      <ul>
+        <li className='app_li'>Puedes ingresar cuantos nombres desees separados por comas (',') o un unico nombre</li>
+        <li className='app_li'>El campo localización es opcional por lo tanto no es necesario que marques el país.</li>
+        <li className='app_li'>Los nombres deben ser unicos, es decir no puedes ingresar: Juan Sebastian</li>
+      </ul>
+      <form className='app_form' onSubmit={handleSubmit}>
+        <InputNames/>
+      </form>
     </div>
   )
 }
---
+++ ejercicio_dos/src/components/InputNames/index.jsx
@@ -0,0 +1,9 @@
+const InputNames = () => {
+
+  return(
+    <input type="text" className="input_name"/>
+  )
+
+}
+
+export default InputNames
